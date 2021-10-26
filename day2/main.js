2344efb60892d5da6916e0fc8488c6037b61d5ed
arturfil <arturfil@hotmail.com>
2021-10-26 12:12:09 -0400

    added new logic with event listeners

--- day2/main.js
+++ day2/main.js
@@ -1,60 +1,82 @@
-
+// constants and variables
 const playersChoiceDisplay = document.getElementById("playersChoice");
 const computersChoiceDisplay = document.getElementById("computersChoice");
 const resulstDisplay = document.getElementById("result");
+// let quit = false;
+let start = false;
+let playersChoice;
 
+// function that sets start to true;
 
-let playersChoice = prompt("Choose Rock = R, Paper = P, Scissors = S").toUpperCase();
-// alert(`You've chosen ${playersChoice}`);
-
-playersChoiceDisplay.innerHTML = playersChoice;
-
-let max = 3;
-let min = 1;
-let computersChoice = Math.floor(Math.random() * (max - min + 1) + min);
-console.log("Number",computersChoice);
-
-switch(computersChoice) {
-  case 3:
-    computersChoice = "R";
-    break;
-  case 2:
-    computersChoice = "S";
-    break;
-  case 1:
-    computersChoice = "P";
-    break;
+const startGame = () => {
+  start = true;
+  console.log("Works")
+  runGame()
 }
 
-computersChoiceDisplay.innerHTML = computersChoice;
+// Loop for game handling logic
+const runGame = () => {
 
-// all possible scenarios when player chooses R;
-if (playersChoice === 'R') {
-  if (computersChoice === 'S') {
-    resulstDisplay.innerHTML = "You Won!";
-  } else if (computersChoice === 'P') {
-    resulstDisplay.innerHTML = "You've Lost!";
-  } else {
-    resulstDisplay.innerHTML = "It's a Draw!";
+  // handle the start of game
+  if (start === true) {
+    playersChoice = prompt("Choose Rock = R, Paper = P, Scissors = S").toUpperCase();
+    playersChoiceDisplay.innerHTML = playersChoice;
   }
+  // alert(`You've chosen ${playersChoice}`);
+
+  // randomizing the computers choice
+  let max = 3;
+  let min = 1;
+  let computersChoice = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log("Number",computersChoice);
+
+  // assigning value of computer
+  switch(computersChoice) {
+    case 3:
+      computersChoice = "R";
+      break;
+    case 2:
+      computersChoice = "S";
+      break;
+    case 1:
+      computersChoice = "P";
+      break;
+  }
+  computersChoiceDisplay.innerHTML = computersChoice;
+
+  // all possible scenarios when player chooses R;
+  if (playersChoice === 'R') {
+    if (computersChoice === 'S') {
+      resulstDisplay.innerHTML = "You Won!";
+    } else if (computersChoice === 'P') {
+      resulstDisplay.innerHTML = "You've Lost!";
+    } else {
+      resulstDisplay.innerHTML = "It's a Draw!";
+    }
+  }
+
+  // scenarios when player chooses S
+  if (playersChoice === 'S') {
+    if (computersChoice === 'P') {
+      resulstDisplay.innerHTML = "You won!";
+    } else if (computersChoice === 'R') {
+      resulstDisplay.innerHTML =  "You've lost";
+    } else {
+      resulstDisplay.innerHTML =  "It's a Draw!";
+    }
+  }
+
+  // scenarios where player chooses P
+  if (playersChoice === 'P') {
+    if (computersChoice === 'R') {
+      resulstDisplay.innerHTML = "You won!";
+    } else if (computersChoice === 'S') {
+      resulstDisplay.innerHTML = "You've lost!";
+    } else {
+      resulstDisplay.innerHTML = "It's a Draw!";
+    }
+  }
+
 }
 
-if (playersChoice === 'S') {
-  if (computersChoice === 'P') {
-    resulstDisplay.innerHTML = "You won!";
-  } else if (computersChoice === 'R') {
-    resulstDisplay.innerHTML =  "You've lost";
-  } else {
-    resulstDisplay.innerHTML =  "It's a Draw!";
-  }
-}
 
-if (playersChoice === 'P') {
-  if (computersChoice === 'R') {
-    resulstDisplay.innerHTML = "You won!";
-  } else if (computersChoice === 'S') {
-    resulstDisplay.innerHTML = "You've lost!";
-  } else {
-    resulstDisplay.innerHTML = "It's a Draw!";
-  }
-}
