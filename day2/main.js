95965cdb20cb7b7d2e76150c3274203a54032ebe
arturfil <arturfil@hotmail.com>
2021-10-26 11:09:06 -0400

    added DOM manipulation

--- day2/main.js
+++ day2/main.js
@@ -1,7 +1,13 @@
 
+const playersChoiceDisplay = document.getElementById("playersChoice");
+const computersChoiceDisplay = document.getElementById("computersChoice");
+const resulstDisplay = document.getElementById("result");
 
-let playersChoice = prompt("Choose Rock = R, Paper = P, Scissors = S");
-alert(`You've chosen ${playersChoice}`);
+
+let playersChoice = prompt("Choose Rock = R, Paper = P, Scissors = S").toUpperCase();
+// alert(`You've chosen ${playersChoice}`);
+
+playersChoiceDisplay.innerHTML = playersChoice;
 
 let max = 3;
 let min = 1;
@@ -20,38 +26,35 @@ switch(computersChoice) {
     break;
 }
 
-alert(`Computer's choice is: ${computersChoice}`)
-
-
-// start with winning scenarios
+computersChoiceDisplay.innerHTML = computersChoice;
 
 // all possible scenarios when player chooses R;
 if (playersChoice === 'R') {
   if (computersChoice === 'S') {
-    alert("You won!");
+    resulstDisplay.innerHTML = "You Won!";
   } else if (computersChoice === 'P') {
-    alert("You've lost");
+    resulstDisplay.innerHTML = "You've Lost!";
   } else {
-    alert("It's a Draw!");
+    resulstDisplay.innerHTML = "It's a Draw!";
   }
 }
 
 if (playersChoice === 'S') {
   if (computersChoice === 'P') {
-    alert("Yo won!")
+    resulstDisplay.innerHTML = "You won!";
   } else if (computersChoice === 'R') {
-    alert("You've lost")
+    resulstDisplay.innerHTML =  "You've lost";
   } else {
-    alert("It's a Draw!")
+    resulstDisplay.innerHTML =  "It's a Draw!";
   }
 }
 
 if (playersChoice === 'P') {
   if (computersChoice === 'R') {
-    alert("You won!");
+    resulstDisplay.innerHTML = "You won!";
   } else if (computersChoice === 'S') {
-    alert("You've lost!")
+    resulstDisplay.innerHTML = "You've lost!";
   } else {
-    alert("It's a Draw!")
+    resulstDisplay.innerHTML = "It's a Draw!";
   }
 }
